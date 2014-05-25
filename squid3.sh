6692648b1cce3aa6f989d1dc5dad6ca2aef5ef64
narrundo <narrundo@users.noreply.github.com>
2014-05-25 19:36:47 +0700

    Create squid3.sh

---
+++ squid3.sh
@@ -0,0 +1,8 @@
+#Squid Proxy 3.1
+apt-get install aptitude
+aptitude -y install squid3
+sed -i 's/#cache_dir/cache_dir/g' /etc/squid3/squid.conf
+wget -O /etc/squid/squid.conf "https://raw.githubusercontent.com/narrundo/vishera/conf/squid.conf"
+service squid3 restart
+
+#ViperSSH Config Only
