f16da8f8f374c53d84a38c637a837944cc8ff08c
narrundo <narrundo@users.noreply.github.com>
2014-05-27 09:47:12 +0700

    Update squid3.sh

--- squid3.sh
+++ squid3.sh
@@ -1,8 +1,9 @@
 #Squid Proxy 3.1
 apt-get install aptitude
 aptitude -y install squid3
-sed -i 's/#cache_dir/cache_dir/g' /etc/squid3/squid.conf
+rm -f /etc/squid3/squid.conf
 wget -P /etc/squid3/ "https://raw.githubusercontent.com/narrundo/vishera/conf/squid.conf"
+sed -i 's/#cache_dir/cache_dir/g' /etc/squid3/squid.conf
 service squid3 restart
 
 #ViperSSH Config Only
