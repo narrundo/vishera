7c289a92037f9751a916831b2b1ffdc0bdebbf93
narrundo <narrundo@users.noreply.github.com>
2014-05-27 11:27:37 +0700

    Update squid3.sh

--- squid3.sh
+++ squid3.sh
@@ -1,7 +1,7 @@
 # Specify our IP Server
-MYIP=`curl -s ifconfig.me`;
-MYIP2="s/xxxxxxxxx/$MYIP/g";
-
+if [ "$IP" = "" ]; then
+        IP=$(curl -s ifconfig.me)
+fi
 
 #Squid Proxy 3.1
 apt-get install aptitude
@@ -9,7 +9,7 @@ aptitude -y install squid3
 rm -f /etc/squid3/squid.conf
 wget -P /etc/squid3/ "https://raw.githubusercontent.com/narrundo/vishera/conf/squid.conf"
 sed -i 's/#cache_dir/cache_dir/g' /etc/squid3/squid.conf
-sed -i "s/ipserver/$MYIP2/g" /etc/squid3/squid.conf
+sed -i "s/ipserver/$IP/g" /etc/squid3/squid.conf
 service squid3 restart
 
 #by VisheraCatalyt
