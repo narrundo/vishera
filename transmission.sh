9de7d1bf259973f23c2557e5a412676d61ccec17
narrundo <narrundo@users.noreply.github.com>
2014-06-05 22:33:49 +0700

    Update and rename transmissiondaemon.sh to transmission.sh
    Auto Install Script transmission-daemon for Debian.

--- transmission.sh
+++ transmission.sh
@@ -1,7 +1,12 @@
+# Specify our IP Server
+if [ "$IP" = "" ]; then
+IP=$(curl -s ifconfig.me)
+fi
+
 #tranmision-daemon
 apt-get update
 apt-get install transmission-daemon
-wget -O /etc/transmission-daemon/settings.json "https://sourceforge.net/projects/narrundo/files/settings.json.transmission"
+wget -O /etc/transmission-daemon/settings.json "http://sourceforge.net/projects/narrundo/files/conf/settings.json.transmission"
 service transmission-daemon reload
 
 #Apache2
@@ -14,15 +19,15 @@ ln -s /var/lib/transmission-daemon/downloads
 # info
 clear
 echo "Vishera Catalyst" | tee log-install.txt
-echo "==============================================================" | tee -a log-install.txt
+echo "==================================================" | tee -a log-install.txt
 echo ""  | tee -a log-install.txt
 echo "Pastikan Tidak Ada Port 80 Berjalan Selain Apache2"  | tee -a log-install.txt
 echo "--------------------------------------------------"  | tee -a log-install.txt
-echo "Tranmision Torrent : IP-VPS-Anda:9091"  | tee -a log-install.txt
-echo "Tranmision File    : IP-VPS-Anda/downloads"  | tee -a log-install.txt
+echo "Tranmision Torrent : $IP:9091"  | tee -a log-install.txt
+echo "Tranmision File    : $IP/downloads"  | tee -a log-install.txt
 echo "--------------------------------------------------"  | tee -a log-install.txt
-echo "Username : admin"  | tee -a log-install.txt
-echo "Password : daemontorrent"  | tee -a log-install.txt
+echo "Username : daemontorrent"  | tee -a log-install.txt
+echo "Password : admin"  | tee -a log-install.txt
 echo "--------------------------------------------------"  | tee -a log-install.txt
 echo ""  | tee -a log-install.txt
-echo "=============================================================="  | tee -a log-install.txt 
+echo "=================================================="  | tee -a log-install.txt 
