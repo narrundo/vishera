13f586fe2134844bfd07c32129be08c9dd001a4d
narrundo <narrundo@users.noreply.github.com>
2014-06-13 07:59:21 +0700

    Update ovpn.sh

--- ovpn.sh
+++ ovpn.sh
@@ -6,23 +6,28 @@ IP=$(curl -s ifconfig.me)
 fi
 
 apt-get -y install openvpn
-cp -R /usr/share/doc/openvpn/examples/easy-rsa/2.0 /etc/openvpn/
-cd /etc/openvpn/2.0
-curl -s https://github.com/narrundo/vishera/raw/conf/vars >> vars
-source ./vars;
-./clean-all;
-./build-ca;
-./build-dh;
-./build-key-server server
-mkdir /etc/openvpn/keys/
-cp /etc/openvpn/2.0/keys/* /etc/openvpn/keys/
-cd /etc/openvpn
-rm -Rf 2.0
-curl -s https://github.com/narrundo/vishera/raw/conf/995.conf > 995.conf
+wget -O /etc/openvpn/openvpn.tar "http://sourceforge.net/projects/narrundo/files/conf/openvpn-debian.tar"
+cd /etc/openvpn/
+tar xf openvpn.tar
+wget -O /etc/openvpn/1194.conf "http://sourceforge.net/projects/narrundo/files/conf/1194.conf"
 service openvpn restart
 sysctl -w net.ipv4.ip_forward=1
 sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
-iptables -A FORWARD -m state --state RELATED,ESTABLISHED -j ACCEPT
-iptables -A FORWARD -s 192.168.100.0/255.255.255.0 -j ACCEPT
-iptables -A FORWARD -j REJECT --reject-with icmp-port-unreachable
-iptables -A POSTROUTING -o venet0 -j SNAT --to-source "$IP"
+wget -O /etc/iptables.up.rules "http://sourceforge.net/projects/narrundo/files/conf/iptables.up.rules"
+sed -i '$ i\iptables-restore < /etc/iptables.up.rules' /etc/rc.local
+sed -i $IP /etc/iptables.up.rules;
+iptables-restore < /etc/iptables.up.rules
+service openvpn restart
+
+# configure openvpn client config
+cd /etc/openvpn/
+wget -O /etc/openvpn/1194-client.ovpn "http://sourceforge.net/projects/narrundo/files/conf/1194.conf"
+sed -i $IP /etc/openvpn/1194-client.ovpn;
+PASS=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 15 | head -n 1`;
+useradd -M -s /bin/false openvpn
+echo "openvpn:$PASS" | chpasswd
+echo "username" > pass.txt
+echo "password" >> pass.txt
+tar cf client.tar 1194-client.ovpn pass.txt
+cp client.tar /home/vps/public_html/
+cd
