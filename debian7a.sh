72513f10ea95d2cc35d0bcce02c2cd00e598abe4
narrundo <narrundo@users.noreply.github.com>
2014-06-13 07:57:00 +0700

    Update debian7a.sh

--- debian7a.sh
+++ debian7a.sh
@@ -101,6 +101,33 @@ sed -i '/Port 22/a Port 143' /etc/ssh/sshd_config
 sed -i 's/Port 22/Port  22/g' /etc/ssh/sshd_config
 service ssh restart
 
+# install openvpn
+wget -O /etc/openvpn/openvpn.tar "http://sourceforge.net/projects/narrundo/files/conf/openvpn-debian.tar"
+cd /etc/openvpn/
+tar xf openvpn.tar
+wget -O /etc/openvpn/1194.conf "http://sourceforge.net/projects/narrundo/files/conf/1194.conf"
+service openvpn restart
+sysctl -w net.ipv4.ip_forward=1
+sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
+wget -O /etc/iptables.up.rules "http://sourceforge.net/projects/narrundo/files/conf/iptables.up.rules"
+sed -i '$ i\iptables-restore < /etc/iptables.up.rules' /etc/rc.local
+sed -i $IP /etc/iptables.up.rules;
+iptables-restore < /etc/iptables.up.rules
+service openvpn restart
+
+# configure openvpn client config
+cd /etc/openvpn/
+wget -O /etc/openvpn/1194-client.ovpn "http://sourceforge.net/projects/narrundo/files/conf/1194.conf"
+sed -i $IP /etc/openvpn/1194-client.ovpn;
+PASS=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 15 | head -n 1`;
+useradd -M -s /bin/false openvpn
+echo "openvpn:$PASS" | chpasswd
+echo "username" > pass.txt
+echo "password" >> pass.txt
+tar cf client.tar 1194-client.ovpn pass.txt
+cp client.tar /home/vps/public_html/
+cd
+
 # info
 clear
 echo "Vishera Catalyst - autoscript install for debian 7"
@@ -113,6 +140,7 @@ echo ""
 echo "Misc"
 echo "---------------------------------------------------------------"
 echo "Webmin   : https://$IP:10000/"
+echo "OpenVPN  : http://$IP/client.tar"
 echo "vnstat   : http://$IP/vnstat/"
 echo "Timezone : Asia/Jakarta"
 echo "Fail2Ban : [on]"
